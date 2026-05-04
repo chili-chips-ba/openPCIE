09ff17a6ab2151fc106db2b19888fdedb2ab20a8
Chili.CHIPS <ask@chili-chips.xyz>
2026-05-04 11:52:29 -0700

    added headers

--- 2.rtl/2.RC-direct.opensource/src/riscv_pcie_soc.v
+++ 2.rtl/2.RC-direct.opensource/src/riscv_pcie_soc.v
@@ -1,5 +1,46 @@
-`timescale 1ns / 1ps
+// SPDX-FileCopyrightText: 2026 Chili.CHIPS*ba
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
+//========================================================================== 
+// openPCIE * NLnet-sponsored open-source implementation   
+//--------------------------------------------------------------------------
+//                   Copyright (C) 2026 Chili.CHIPS*ba
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright 
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright 
+// notice, this list of conditions and the following disclaimer in the 
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its 
+// contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//              https://opensource.org/license/bsd-3-clause
+//--------------------------------------------------------------------------
+// Description:
+//   opensource openPCIE RISC-V SOC that replaces AMD-proprietary FSM
+//==========================================================================
+
+`timescale 1ns / 1ps
 
 module riscv_pcie_soc (
     input wire clk,
@@ -210,4 +251,10 @@ module riscv_pcie_soc (
 
     assign mem_ready = ram_ready || bridge_ready_reg;
 
-endmodule
+endmodule // riscv_pcie_soc
+
+//--------------------------------------------------------------------------
+// Revision history:
+//  2026/01/10 AV - initial creation
+//  2026/02/03 AV - updates based on test results on hardware
+//--------------------------------------------------------------------------
